8aeb37a7d087221c8b0b7f9f2443c51c053189ae
Nikita <saininikita23@gmail.com>
2022-01-17 15:51:49 +0530

    unique user ids

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { connect} from 'react-redux';
-import { fetchUser } from '../actions';
+
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
+    
     render() {
         const { user} = this.props;
-        
+
         if(!user) {
             return null;
         }
@@ -20,4 +18,4 @@ class UserHeader extends React.Component {
 const mapStateToProps = (state, ownProps) => {
  return {users: state.users.find(user => user.id === ownProps.userId )} ;
 };
-export default connect(mapStateToProps, {fetchUser}) (UserHeader);
+export default connect(mapStateToProps) (UserHeader);
